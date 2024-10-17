3bcaf268f738805c8c41015c30db64dc00dc9096
osos <elliotosama57@gmail.com>
2024-10-17 20:48:16 +0300

    making some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import Card from './Pages/Card';
 import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -14,15 +18,16 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/men" element={<ShopCategory category="men"/>}/>
-          <Route path="/women" element={<ShopCategory category="women"/>}/>
-          <Route path="/kids" element={<ShopCategory category="kids"/>}/>
+          <Route path="/men" element={<ShopCategory banner={men_banner} category="men"/>}/>
+          <Route path="/women" element={<ShopCategory banner={women_banner} category="women"/>}/>
+          <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kids"/>}/>
           <Route path='/product' element={<Product/>}>
             <Route path=':id' element={<Product/>}/>
           </Route>
           <Route path='/card' element={<Card/>}/>
           <Route path='/login' element={<LoginSignup/>}/>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -2,7 +2,23 @@ import React from "react";
 
 const LoginSignup = () => {
   return (
-    <div>
+    <div className="loginSignup">
+      <div className="loginSignup-container">
+        <h1>Sign Up</h1>
+        <div className="fields">
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <a href="#">Login</a>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name='' id=''/>
+            <p>By continueing, i agree to the terms of use & privacy policy.</p>
+          </div>
+        </p>
+      </div>
     </div>
   )
 }
